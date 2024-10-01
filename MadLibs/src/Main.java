9918aa0045fd16b1e99290559a0e56d35111d06b
GreasyRooster <greasyRooster1@gmail.com>
2024-10-01 08:47:37 -0700

    madlib data

--- MadLibs/src/Main.java
+++ MadLibs/src/Main.java
@@ -1,4 +1,10 @@
 public class Main {
+    public static String story = "wow im programming in {0}. This language is so {1}. Programming makes" +
+            "me super {2}. I use {3} btw, and i love spending {4} hours every day fixing random stuff." +
+            "i use {5} to write code, because using a mouse is overrated. im such a {6} programmer." +
+            "ive always wanted to make a {7} in {8} using {9}. anyways, im going to get back to" +
+            "drinking my {10} latte and debugging with stack overflow, {11}";
+    public static String[] questionWords = {"Programming Language","adjective","negative feeling","linux distro","number","code editor","adjective","programming project","programming language","sci-fi technology","type of milk","customary programmer goodbye"};
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
