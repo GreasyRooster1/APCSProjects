d66d5800d1569f9f00e0479cc0b781f4d48b26f8
GreasyRooster <greasyRooster1@gmail.com>
2025-02-13 09:00:46 -0800

    first try again

--- Hangman/src/Renderer.java
+++ Hangman/src/Renderer.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 
 public class Renderer {
-    public final static int WIDTH = 60;
-    public final static int HEIGHT = 20;
+    public final static int WIDTH = 30;
+    public final static int HEIGHT = 10;
 
     public void render(GameState state){
         ArrayList<ArrayList<Character>> screen = new ArrayList<>();
@@ -13,10 +13,13 @@ public class Renderer {
             }
             screen.add(tmp);
         }
+
+        renderMan(screen);
         renderBorder(screen);
 
         print(screen);
     }
+
     public void print(ArrayList<ArrayList<Character>> screen) {
         StringBuilder out = new StringBuilder();
         for (int i = 0; i < HEIGHT; i++) {
@@ -26,6 +29,10 @@ public class Renderer {
             out.append("\n");
         }
         System.out.println(out);
+    }
+
+    public void renderMan(ArrayList<ArrayList<Character>> screen){
+        screen.get(2).set(4,'0');
     }
 
     public void renderBorder(ArrayList<ArrayList<Character>> screen){
